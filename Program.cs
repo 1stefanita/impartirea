acb7dcea1031bb36efe1104da385228d71177a6f
1stefanita <146415173+1stefanita@users.noreply.github.com>
2023-10-16 00:20:14 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿  namespace impartire
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                int x, y, z;
+
+                x = Convert.ToInt32(Console.ReadLine());
+                y = Convert.ToInt32(Console.ReadLine());
+                z = x / y;
+
+                if (y != 0 && x % y == 0)
+                {
+                    Console.WriteLine(z);
+                }
+                else
+                {
+                    Console.WriteLine("impartirea nu poate fi efectuata");
+
+                }
+
+            }
+        }
+    }
+
+    
